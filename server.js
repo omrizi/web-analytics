a52cdc08bbed1f2c026d389b91900676b76c5be4
root <root@ip-172-31-93-183.ec2.internal>
2020-03-04 05:59:33 +0000

    fakeoziner changes from inside aws server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//v2
 'use strict';
 
 const express = require('express');
@@ -9,8 +10,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('This server powered by Omri Ziner,The instance is a aws ec2 instance, on the server we run nodejs webserver on docker container. Yogi & Goku - start working!');
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log('Running on http://${HOST}:${PORT}');
