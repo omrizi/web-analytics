569b6f5d6eb22a3c895398b78b77f0ee0c184117
fakeoziner <fakeoziner@gmail.com>
2020-03-04 08:10:37 +0200

    changes in server and Dockercommand file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('This site powered by OmriZiner.The instance is aws@ec2. the server is nodejs running in docker container tec');
 });
 
 app.listen(PORT, HOST);
