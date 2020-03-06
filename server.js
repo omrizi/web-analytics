7ff1e8e31df4e4d40e791f58a8b0ce7e41499c12
fakeoziner <fakeoziner@gmail.com>
2020-03-06 10:10:53 +0200

    Update server.js
    add Date parameters

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
+var d = new Date();
 const app = express();
 app.get('/', (req, res) => {
-  res.send('This site powered by OmriZiner.The instance is aws@ec2. the server is nodejs running in docker container tec');
+  res.send('This site powered by OmriZiner.The instance is aws@ec2. the server is nodejs running in docker container tec'+ Date);
 });
 
 app.listen(PORT, HOST);
